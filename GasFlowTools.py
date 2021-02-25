3cc9af4c39a67537704d90e2b5e9c847bfcaf6aa
Ruby Wright <21486778@student.uwa.edu.au>
2021-02-25 09:29:28 +0800

    Create GasFlowTools.py

---
+++ GasFlowTools.py
@@ -0,0 +1,58 @@
+import os
+import time
+import logging
+import h5py
+import numpy as np
+import pandas as pd
+
+def extract_tree(path,mcut,snapidxmin=0):
+
+    outname='catalogue_tree.hdf5'
+    fields=['snapshotNumber','fofIndex','hostIndex','nodeIndex','descendantIndex','mainProgenitorIndex']
+    mcut=10**mcut/10**10 
+
+    if not os.path.exists('jobs'):
+        os.mkdir('jobs')
+    
+    if os.path.exists('jobs/extract_tree.log'):
+        os.remove('jobs/extract_tree.log')
+
+    logging.basicConfig(filename='jobs/extract_tree.log', level=logging.INFO)
+    logging.info(f'Running tree extraction for haloes with mass above {mcut*10**10:.1e} after snapidx {snapidxmin} ...')
+
+    # get file names
+    tree_fnames=os.listdir(path)
+    nfiles=len(tree_fnames)
+
+    # iterate through all tree files
+    t0=time.time()
+    for ifile,tree_fname in enumerate(tree_fnames):
+        print(f'Processing file {ifile+1} of {nfiles}')
+        treefile=h5py.File(f'{path}/{tree_fname}')
+
+        #mass mask
+        masses=treefile['/haloTrees/nodeMass'][:];snipshotidx=treefile['/haloTrees/snapshotNumber'][:]
+        mask=np.logical_and(masses>mcut,snipshotidx>=snapidxmin)
+
+        #initialise new data
+        logging.info(f'Extracting position for {np.sum(mask):.0f} nodes [runtime {time.time()-t0:.2f} sec]')
+        newdata=pd.DataFrame(treefile['/haloTrees/position'][mask,:],columns=['position_x','position_y','position_z'])
+
+        #grab all fields
+        logging.info(f'Extracting data for {np.sum(mask):.0f} nodes [runtime {time.time()-t0:.2f} sec]')
+        newdata.loc[:,fields]=np.column_stack([treefile['/haloTrees/'+field][mask] for field in fields])
+
+        #append to data frame
+        if ifile==0:
+            data=newdata
+        else:
+            data=data.append(newdata,ignore_index=True)
+
+
+        #close file, move to next
+        treefile.close()
+
+    if os.path.exists(outname):
+        os.remove(outname)
+        
+    data.to_hdf(f'{outname}',key='Tree')
