a1240df242e4bbf2456759373123729b84e054eb
Ruby Wright <21486778@student.uwa.edu.au>
2021-02-26 13:58:30 +0800

    Adding fof extraction

--- GasFlowTools.py
+++ GasFlowTools.py
@@ -8,7 +8,16 @@ import pandas as pd
 def extract_tree(path,mcut,snapidxmin=0):
 
     outname='catalogues/catalogue_tree.hdf5'
-    fields=['snapshotNumber','fofIndex','hostIndex','nodeIndex','descendantIndex','mainProgenitorIndex']
+    fields=['snapshotNumber',
+            'nodeIndex',
+            'fofIndex',
+            'hostIndex',
+            'enclosingIndex',
+            'isFoFCentre',
+            'descendantIndex',
+            'mainProgenitorIndex',
+            'positionInCatalogue']
+
     mcut=10**mcut/10**10 
 
     if not os.path.exists('logs'):
@@ -17,8 +26,8 @@ def extract_tree(path,mcut,snapidxmin=0):
     if not os.path.exists('catalogues'):
         os.mkdir('catalogues')
     
-    if os.path.exists('jobs/extract_tree.log'):
-        os.remove('jobs/extract_tree.log')
+    if os.path.exists('logs/extract_tree.log'):
+        os.remove('logs/extract_tree.log')
 
     logging.basicConfig(filename='logs/extract_tree.log', level=logging.INFO)
     logging.info(f'Running tree extraction for haloes with mass above {mcut*10**10:.1e} after (and including) snapidx {snapidxmin} ...')
@@ -60,3 +69,92 @@ def extract_tree(path,mcut,snapidxmin=0):
         os.remove(outname)
         
     data.to_hdf(f'{outname}',key='Tree')
+
+def extract_fofs(path,mcut,isnip=True,snapidxmin=0):
+    outname='catalogues/catalogue_fof.hdf5'
+    fields=['/FOF/GroupMass',
+            '/FOF/Group_M_Crit200',
+            '/FOF/Group_R_Crit200',
+            '/FOF/NumOfSubhalos',
+            '/FOF/GroupCentreOfPotential']
+    mcut=10**mcut/10**10 
+    redshift_table=pd.read_hdf('snipshot_redshifts.hdf5',key='snipshots')
+    snapshots_valid=redshift_table.loc[redshift_table['snapshotidx']>=0,'snapshot'].values
+    dims='xyz'
+
+    groupdirs=os.listdir(path)
+    groupdirs=sorted([path+'/'+groupdir for groupdir in groupdirs if ('groups_snip' in groupdir and 'tar' not in groupdir)])
+
+    logging.basicConfig(filename='logs/extract_fof.log', level=logging.INFO)
+    logging.info(f'Running fof extraction for FOFs with mass above {mcut*10**10:.1e} after (and including) snapidx {snapidxmin} ...')
+
+    t0=time.time()
+    for isnap,groupdir in enumerate(groupdirs):
+        snap=int(groupdir.split('z')[0][:-3])
+        snapidx=redshift_table.loc[snap==redshift_table['snapshot'],'snapshotidx']
+        
+        if snapidx>=snapidxmin:
+            logging.info(f'Processing snap {snapidx} [runtime {time.time()-t0:.2f} sec]')
+            groupdirfnames=os.listdir(groupdir)
+            groupdirfnames=sorted([groupdir+'/'+groupdirfname for groupdirfname in groupdirfnames if groupdirfname.startswith('eagle_subfind')])
+            groupdirfnames_n=len(groupdirfnames)
+
+            for ifile_snap, groupdirfname in enumerate(groupdirfnames):
+                groupdirifile=h5py.File(groupdirfname,'r')
+
+                ifile_fofmasses=groupdirifile['/FOF/GroupMass'].value
+                ifile_mask=ifile_fofmasses>mcut
+                ifile_nfof=np.sum(ifile_mask)
+    
+                logging.info(f'Snap {snapidx}, file {ifile_snap}/{groupdirfnames_n}: extracting data for {ifile_nfof:.0f} FOFs [runtime {time.time()-t0:.2f} sec]')
+                
+                newdata=pd.DataFrame(groupdirifile['/FOF/GroupMass'][ifile_mask],columns=['GroupMass'])
+                newdata.loc[:,'snapshotidx']=snapidx
+
+                for field in fields:
+                    dset_shape=groupdirifile[field].shape
+                    if len(dset_shape)==2:
+                        for icol in range(dset_shape[-1]):
+                            if dset_shape[-1]==3:
+                                newdata.loc[:,field.split('FOF/')[-1]+f'_{dims[icol]}']=groupdirifile[field][ifile_mask,icol]
+                            else:
+                                if icol in [0,1,4,5]:
+                                    newdata.loc[:,field.split('FOF/')[-1]+f'_{icol}']=groupdirifile[field][ifile_mask,icol]
+                else:
+                    newdata.loc[:,field.split('FOF/')[-1]]=groupdirifile[field][ifile_mask]
+
+            if isnap==0 and ifile_snap==0:
+                data=newdata
+            else:
+                data=data.append(newdata,ignore_index=True)
+    
+    if os.path.exists(f'catalogues/{outname}'):
+        os.remove(f'catalogues/{outname}')
+
+    data.to_hdf(f'catalogues/{outname}',key='FOF')
+
+
+
+
+
+
+
+# def extract_subs(path,mcut,snapidxmin=0):
+
+#     outname='catalogues/catalogue_subhalo.hdf5'
+#     fields=['/Subhalo/GroupNumber',
+#             '/Subhalo/SubGroupNumber',
+#             '/Subhalo/MassType',
+#             '/Subhalo/ApertureMeasurements/Mass/030kpc',
+#             '/Subhalo/Vmax',
+#             '/Subhalo/CentreOfPotential',
+#             '/Subhalo/Velocity',
+#             '/Subhalo/CentreOfMass',
+#             '/Subhalo/HalfMassRad']
+    
+
+
+
+    
+    
+#
