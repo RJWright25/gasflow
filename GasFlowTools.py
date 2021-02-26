cfdd501ea956954fe41a8e866b0e92107d332c77
Ruby Wright <21486778@student.uwa.edu.au>
2021-02-26 15:01:16 +0800

    Adding subhalo extraction

--- GasFlowTools.py
+++ GasFlowTools.py
@@ -147,7 +147,86 @@ def extract_fof(path,mcut,snapidxmin=0):
     data.to_hdf(f'{outname}',key='FOF')
 
 
+def extract_subhalo(path,mcut,snapidxmin=0):
+    outname='catalogues/catalogue_subhalo.hdf5'
+    fields=['/Subhalo/GroupNumber',
+            '/Subhalo/SubGroupNumber',
+            '/Subhalo/Mass',
+            '/Subhalo/MassType',
+            '/Subhalo/ApertureMeasurements/Mass/030kpc',
+            '/Subhalo/Vmax',
+            '/Subhalo/CentreOfPotential',
+            '/Subhalo/Velocity',
+            '/Subhalo/CentreOfMass',
+            '/Subhalo/HalfMassRad']
 
+    mcut=10**mcut/10**10 
+    redshift_table=pd.read_hdf('snapshot_redshifts.hdf5',key='snapshots')
+    dims='xyz'
+
+    groupdirs=os.listdir(path)
+    groupdirs=sorted([path+'/'+groupdir for groupdir in groupdirs if ('groups_snip' in groupdir and 'tar' not in groupdir)])
+
+    if os.path.exists('logs/extract_subhalo.log'):
+        os.remove('logs/extract_subhalo.log')
+
+    logging.basicConfig(filename='logs/extract_subhalo.log', level=logging.INFO)
+    logging.info(f'Running subhalo extraction for subhaloes with mass above {mcut*10**10:.1e} after (and including) snapidx {snapidxmin} ...')
+
+    t0=time.time()
+    ifile=0
+    isnap=-1
+    for groupdir in groupdirs:
+        snap=int(groupdir.split('snip_')[-1][:3])
+        try:
+            snapidx=redshift_table.loc[snap==redshift_table['snapshot'],'snapshotidx'].values[0]
+        except:
+            logging.info(f'Skipping snap {snapidx} (not in trees) [runtime {time.time()-t0:.2f} sec]')
+            continue
+
+        if snapidx>=snapidxmin:
+            isnap+=1
+            logging.info(f'Processing snap {snapidx} ({isnap+1}/{len(groupdir)} total) [runtime {time.time()-t0:.2f} sec]')
+            groupdirfnames=os.listdir(groupdir)
+            groupdirfnames=sorted([groupdir+'/'+groupdirfname for groupdirfname in groupdirfnames if groupdirfname.startswith('eagle_subfind')])
+            groupdirfnames_n=len(groupdirfnames)
+
+            for ifile_snap, groupdirfname in enumerate(groupdirfnames):
+                groupdirifile=h5py.File(groupdirfname,'r')
+
+                ifile_submasses=groupdirifile['/Subhalo/Mass'].value
+                ifile_mask=ifile_submasses>mcut
+                ifile_nfof=np.sum(ifile_mask)
+    
+                logging.info(f'Snap {snapidx} ({isnap+1}/{len(groupdir)} total), file {ifile_snap+1}/{groupdirfnames_n}: extracting data for {ifile_nfof:.0f} FOFs [runtime {time.time()-t0:.2f} sec]')
+                
+                if ifile_nfof:
+                    newdata=pd.DataFrame(groupdirifile['/Subhalo/Mass'][ifile_mask],columns=['Mass'])
+                    newdata.loc[:,'snapshotidx']=snapidx
+
+                    for field in fields:
+                        dset_shape=groupdirifile[field].shape
+                        if len(dset_shape)==2:
+                            for icol in range(dset_shape[-1]):
+                                if dset_shape[-1]==3:
+                                    newdata.loc[:,field.split('Subhalo/')[-1]+f'_{dims[icol]}']=groupdirifile[field][ifile_mask,icol]
+                                else:
+                                    if icol in [0,1,4,5]:
+                                        newdata.loc[:,field.split('Subhalo/')[-1]+f'_{icol}']=groupdirifile[field][ifile_mask,icol]
+                        else:
+                            newdata.loc[:,field.split('Subhalo/')[-1]]=groupdirifile[field][ifile_mask]
+
+                    if ifile==0:
+                        data=newdata
+                    else:
+                        data=data.append(newdata,ignore_index=True)
+
+                    ifile+=1
+        
+    if os.path.exists(f'{outname}'):
+        os.remove(f'{outname}')
+
+    data.to_hdf(f'{outname}',key='Subhalo')
 
 
 
