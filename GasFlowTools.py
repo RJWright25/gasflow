3fa8e5fbd47b7650a9ba6f6f2f57f492ead5cbce
Ruby Wright <21486778@student.uwa.edu.au>
2021-03-04 14:54:04 +0800

    Chaning galaxy & halo definitions

--- GasFlowTools.py
+++ GasFlowTools.py
@@ -675,8 +675,8 @@ def analyse_gasflow(path,mcut,snapidx,nvol,ivol,snapidx_delta=1):
             del part_data_candidates_snap2[dset]
 
         #ism def
-        ism_snap1=np.logical_or(part_data_candidates_snap1["StarFormationRate"].values>0,np.logical_and.reduce([part_data_candidates_snap1["r_com"].values<hmsradius*4,part_data_candidates_snap1["Temperature"].values<10**5,part_data_candidates_snap1["Density"].values*nh_conversion<0.1]))
-        ism_snap2=np.logical_or(part_data_candidates_snap2["StarFormationRate"].values>0,np.logical_and.reduce([part_data_candidates_snap2["r_com"].values<hmsradius*4,part_data_candidates_snap2["Temperature"].values<10**5,part_data_candidates_snap2["Density"].values*nh_conversion<0.1]))
+        ism_snap1=np.logical_or(part_data_candidates_snap1["Density"].values>1,np.logical_and.reduce([part_data_candidates_snap1["r_com"].values<hmsradius*4,part_data_candidates_snap1["Temperature"].values<10**5,part_data_candidates_snap1["Density"].values*nh_conversion>0.1]))
+        ism_snap2=np.logical_or(part_data_candidates_snap2["Density"].values>1,np.logical_and.reduce([part_data_candidates_snap2["r_com"].values<hmsradius*4,part_data_candidates_snap2["Temperature"].values<10**5,part_data_candidates_snap2["Density"].values*nh_conversion>0.1]))
 
         #new ism particles
         ism_partidx_in=np.logical_and(np.logical_not(ism_snap1),ism_snap2)
