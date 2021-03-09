c4bd4d54a5e81840b137c147a7a2e1dbdbaf94d3
Ruby Wright <21486778@student.uwa.edu.au>
2021-03-09 14:22:27 +0800

    debugging combine catalogues function

--- GasFlowTools.py
+++ GasFlowTools.py
@@ -825,6 +825,7 @@ def combine_catalogues(nvol,mcut,snapidxs=[],snapidx_deltas=[1]):
                 print(f'Loading file {isub_snap+1}/{nvol**3} for snap {snapidx} delta {delta}')
                 try:
                     accfile_data_file=pd.read_hdf(f'catalogues/gasflow/gasflow_snapidx_{snapidx}_delta_{str(delta).zfill(3)}_n_{str(nvol).zfill(2)}_volume_{str(ivol).zfill(3)}.hdf5',key='Flux')
+                    print(list(accfile_data_file))
                 except:
                     print(f'Could not load volume {ivol}')
                     continue
