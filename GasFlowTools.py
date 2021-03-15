6254ba21155791ed97286d6524db85708ca02fe5
Ruby Wright <21486778@student.uwa.edu.au>
2021-03-15 13:18:26 +0800

    adding analyse subhalo function

--- GasFlowTools.py
+++ GasFlowTools.py
@@ -579,8 +579,7 @@ def analyse_subhalo(path,mcut,snapidx,nvol,ivol):
         totbaryonmass=np.nansum(part_data_candidates.loc[:,"Mass"])
         
         #fit baryon mass profile
-        ncomp=int(np.floor(npart_galaxy/20))
-        r200_bins=np.concatenate([np.linspace(0,0.095,20),np.linspace(0.1,1,ncomp)])
+        r200_bins=np.linspace(0,1,200)
         r200_bins_mid=r200_bins[1:]
         masks=[np.logical_and(part_data_candidates["rrel_com"]>bin_lo,part_data_candidates["rrel_com"]<bin_hi) for bin_lo, bin_hi in zip(r200_bins[:-1],r200_bins[1:])]
         mass_binned=[np.nansum(part_data_candidates.loc[mask,"Mass"]) for mask in masks]
